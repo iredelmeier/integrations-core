e2db0af1c9c6a812c132492a5579d6f97881c40e
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2023-01-24 09:17:55 +0100

    Postgres: Add `postgres.transactions.xid` metric to report global txid
    Report the global txid extracted from `pg_snapshot_xmin(pg_current_snapshot())`.
    This will provide a way to see the rate of txid consumption.
    
    The current `postgresql.commits` only provides visibility on commits
    which include both virtual and non-virtual transactions.
    `postgres.transactions.xid` only reports transactions that were
    assigned txid when they doing changes in the database.

--- postgres/datadog_checks/postgres/postgres.py
+++ postgres/datadog_checks/postgres/postgres.py
@@ -29,6 +29,9 @@ from .util import (
     REPLICATION_METRICS,
     SLRU_METRICS,
     DatabaseConfigurationError,  # noqa: F401
+    TX_METRICS,
+    TX_METRICS_LT_13,
+    DatabaseConfigurationError,
     fmt,
     get_schema_field,
 )
@@ -384,6 +387,9 @@ class PostgreSql(AgentCheck):
             metric_scope.append(self.metrics_cache.get_count_metrics())
         if self.version >= V13:
             metric_scope.append(SLRU_METRICS)
+            metric_scope.append(TX_METRICS)
+        if self.version < V13:
+            metric_scope.append(TX_METRICS_LT_13)
 
         # Do we need relation-specific metrics?
         if self._config.relations:
